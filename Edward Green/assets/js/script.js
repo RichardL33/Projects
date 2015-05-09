f0547e73ffad532ab62e7b940d247fd738d8c492
Richard L33 <richardlee454@gmail.com>
2015-05-09 02:16:26 -0400

    tablet & desktop

--- Edward Green/assets/js/script.js
+++ Edward Green/assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $('#left-menu').sidr({
       name: 'sidr-left',
       side: 'left' // By default
@@ -7,4 +8,18 @@ $(document).ready(function() {
       name: 'sidr-right',
       side: 'right'
     });
+
+$(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('#back_top').fadeIn('slow');
+        } else {
+            $('#back_top').fadeOut('slow');
+        }
+    });
+    $('#back_top').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        //$("html, body").scrollTop(0); //For without animation
+        return false;
+    });
+
 });
