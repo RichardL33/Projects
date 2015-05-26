952693be4c44d9596a8e17897463e38f78a57f85
Richard L33 <richardlee454@gmail.com>
2015-05-25 22:29:05 -0400

    768 animations

--- Sok/assets/js/script.js
+++ Sok/assets/js/script.js
@@ -2,13 +2,13 @@
 
 function checkSize(){
     if ($(".main_container").css("width") === "320px" ) {
-        // console.log("ONe");
+        // console.log("320");
     } else if ($(".main_container").css("width") === "480px" ) {
-    	// console.log("Twooos");
+    	// console.log("480");
     } else if ($(".main_container").css("width") === "768px" ) {
-    	// console.log("threrwes");
+    	// console.log("768");
     } else {
-    	// console.log("Fsdfouasodrs");
+    	// console.log("ELSE");
     }
 }
 
@@ -21,7 +21,6 @@ $(document).ready(function() {
     $(window).resize(checkSize);
 
     // SHOW BUTTON ACTIONS
-
     $("#show_off").click(function(){
     	$(this).css(
     		"display", "none"
@@ -33,3 +32,173 @@ $(document).ready(function() {
 
     });
 });
+
+
+$(document).ready(function() {
+        var controller = $.superscrollorama({
+            isVertical: true,
+            triggerAtCenter: false,
+            playoutAnimations: true,
+            reverse: true
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_1'), 1, 
+                {css:{rotationX: -90}, immediateRender:true}, 
+                {css:{rotationX: 0}})
+            )
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_3'), 1, 
+                {css:{top: 31}, immediateRender:true}, 
+                {css:{top: 62}})
+            ),
+            offset: 250
+        });
+
+        controller.pin($('#group_text_1'), 200, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_4'), 1, 
+                {css:{left: -450}, immediateRender:true}, 
+                {css:{left: 195}})
+            ),
+            offset: 200
+        });
+
+        controller.pin($('#group_text_1'), 200, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_5'), 1, 
+                {css:{opacity: 0}, immediateRender:true}, 
+                {css:{opacity: 1}})
+            )
+            .append(
+              TweenMax.fromTo($('#animated_text_5'), 0.5, 
+                {css:{top: -35}, immediateRender:true}, 
+                {css:{top: 0}})
+            ),
+            offset: 500
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_6'), 1, 
+                {css:{right: -300}, immediateRender:true}, 
+                {css:{right: 230}})
+            ),
+            offset: 750
+        });
+
+        controller.pin($('#group_text_1'), 200, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_7'), 0.5, 
+                {css:{opacity: 0}, immediateRender:true}, 
+                {css:{opacity: 1}})
+            )
+            .append(
+              TweenMax.fromTo($('#animated_text_7'), 1, 
+                {css:{top: 113}, immediateRender:true}, 
+                {css:{top: 43}})
+            ),
+            offset: 900
+        });
+
+        controller.pin($('#group_text_1'), 200, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_8'), 1, 
+                {css:{opacity: 0}, immediateRender:true}, 
+                {css:{opacity: 1}})
+            )
+            .append(
+              TweenMax.fromTo($('#animated_text_8'), 1, 
+                {css:{left: 0}, immediateRender:true}, 
+                {css:{left: 237}})
+            ),
+            offset: 1100
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_9'), 1, 
+                {css:{right: -355}, immediateRender:true}, 
+                {css:{right: 214}})
+            ),
+            offset: 1350
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_10'), 1, 
+                {css:{rotationX: -90}, immediateRender:true}, 
+                {css:{rotationX: 0}})
+            ),
+            offset: 1450
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_11'), 0.5, 
+                {css:{opacity: 0}, immediateRender:true}, 
+                {css:{opacity: 1}})
+            )
+            .append(
+              TweenMax.fromTo($('#animated_text_11'), 1, 
+                {css:{left: -30}, immediateRender:true}, 
+                {css:{left: 230}})
+            ),
+            offset: 1500
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_12'), 1, 
+                {css:{rotationX: -90}, immediateRender:true}, 
+                {css:{rotationX: 0}})
+            ),
+            offset: 1700
+        });
+
+        controller.pin($('#group_text_1'), 100, {
+          anim: (new TimelineLite())
+            .append(
+              TweenMax.fromTo($('#animated_text_13'), 0.05, 
+                {css:{opacity: 0}, immediateRender:true}, 
+                {css:{opacity: 1}})
+            )
+            .append(
+              TweenMax.fromTo($('#animated_text_13'), 1, 
+                {css:{top: 95}, immediateRender:true}, 
+                {css:{top: 245}})
+            ),
+            offset: 1900
+        });
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sok/assets/js/untitled.js
@@ -0,0 +1,56 @@
+        // controller.addTween('#animated_text_1', 
+        // TweenMax.fromTo($('#animated_text_1'), 0.2, 
+        // {
+        //     css:{
+        //         rotationX:-90
+        //     }, immediateRender:true},
+        //     {
+        //     css:{
+        //         rotationX:0
+        //     }
+        // }), -800);
+
+        // controller.addTween('#animated_text_1',
+        // TweenMax.fromTo($('#animated_text_1'), 1, {
+        //  rotationX: -90
+        // },   
+        // {
+        //  rotationX: 0,
+        // }), -800);
+
+        // controller.addTween('#animated_text_1', 
+        //     TweenMax.fromTo( $('#animated_text_1'), 0.5, 
+        //         {css:{rotationX: -90}, immediateRender:true, ease:Quad.easeInOut}, 
+        //         {css:{rotationX: 0}, ease:Quad.easeInOut}), 
+        //     0, 
+        //     -800);
+
+       // controller.addTween('#animated_text_1', 
+       //   TweenMax.fromTo($('#animated_text_1'), 1, 
+       //     {css:{rotationX: -90}, immediateRender:true}, 
+       //     {css:{rotationX: 0}}),
+       //     0, // scroll duration of tween (0 means autoplay)
+       //     -200); // offset the start of the tween by 200 pixels
+
+       // controller.addTween('#animated_text_3', 
+       //   TweenMax.fromTo($('#animated_text_3'), 1, 
+       //     {css:{top: 31}, immediateRender:true}, 
+       //     {css:{top: 62}}),
+       //     0, // scroll duration of tween (0 means autoplay)
+       //     -100); // offset the start of the tween by 200 pixels
+
+
+
+        // controller.addTween(
+        //       '#group_text_1',
+        //       (new TimelineLite())
+        //         .append([
+        //           TweenMax.fromTo($('#animated_text_1'), 1, 
+        //             {css:{rotationX: -90}, immediateRender:true}, 
+        //             {css:{rotationX: 0}}),
+        //           TweenMax.fromTo($('#animated_text_3'), 0.5, 
+        //             {css:{top: 31}, immediateRender:true}, 
+        //             {css:{top: 62}})
+        //         ]),
+        //       1000 // scroll duration of tween
+        //     );
