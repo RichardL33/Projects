4f4a084bca4e2cb201b346a7b5213fe4c403516e
Richard Lee <richard.lee@digitas.com>
2018-06-18 23:27:40 -0400

    Clean up with favicons

---
+++ Portobello/assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('.project-card__link a, .project-card__description a').click(function(event){
+      event.stopPropagation();
+  });
+  $('.row.project-card').on('click',function(){
+    var a = $(this).attr('data-target');
+    var b = $(a).find('.portfolio-slider')
+    var slickParam = {
+            autoplay: true,
+            autoplaySpeed: 5000,
+            pauseOnHover: true,
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            adaptiveHeight: true
+          };
+    setTimeout(function(){
+      $(a).find('.portfolio-slider').each(function(){
+        if (!$(this).hasClass('slick-initialized')) {
+          b.slick(slickParam);
+        } 
+        else if ($(this).hasClass('slick-initialized')) {
+          b.slick('destroy');
+          b.slick(slickParam);
+        }
+      });
+    }, 200);
+  });
+})
